e5b2cf44d42a6babec078cd7bde2f646cffcbe77
practicaluser <dlcnsdncjswo@naver.com>
2025-10-07 21:43:56 +0900

    localhost에서 127.0.0.1로 명시적으로 변경

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # mysql+mysqlconnector → mysql+pymysql 로 변경
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/mydatabase?charset=utf8mb4"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@127.0.0.1:3306/mydatabase?charset=utf8mb4"
 
 # engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
